72cfbb57ac9f0146837de009254425a057f9342a
capybarin <vladibzd@gmail.com>
2020-10-09 16:37:13 +0300

    Updated exceptions

--- src/main/java/com/example/Library/exception/AuthorNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.example.Library.exception;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(value= HttpStatus.NOT_FOUND)
-public class AuthorNotFoundException extends RuntimeException {
-    public AuthorNotFoundException (Integer id) {super("Author with id \"" + id + "\" not found");}
-}
--- src/main/java/com/example/Library/exception/NotFoundException.java
+++ src/main/java/com/example/Library/exception/NotFoundException.java
@@ -4,8 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
-public class TagNotFoundException extends RuntimeException {
-    public TagNotFoundException(Integer id){
-        super("Tag with id \"" + id + "\" not found");
+public class NotFoundException extends RuntimeException {
+    public NotFoundException (String item, Integer id){
+        super(item + " with id \"" + id + "\" not found");
     }
 }
